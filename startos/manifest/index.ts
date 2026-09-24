8b171e62f46ebf55c43a2b60a1018fa5b727f3e5
lioranecho <support.satslist@gmail.com>
2026-09-24 11:11:53 -0400

    Bump to 0.1.7: add session-based API authentication

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -13,7 +13,7 @@ export const manifest = setupManifest({
   volumes: ['data'],
   images: {
     lcc: {
-      source: { dockerTag: 'sparkielabs/lightning-control-center:0.1.6' },
+      source: { dockerTag: 'sparkielabs/lightning-control-center:0.1.7' },
       arch: ['x86_64', 'aarch64'],
     },
   },

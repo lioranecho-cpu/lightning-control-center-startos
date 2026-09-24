04930569c098b406817bc1342126f38ca9322e7d
lioranecho <support.satslist@gmail.com>
2026-09-24 09:58:50 -0400

    Bump Docker image to 0.1.5, scrub sensitive files from image

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -13,7 +13,7 @@ export const manifest = setupManifest({
   volumes: ['data'],
   images: {
     lcc: {
-      source: { dockerTag: 'sparkielabs/lightning-control-center:0.1.4' },
+      source: { dockerTag: 'sparkielabs/lightning-control-center:0.1.5' },
       arch: ['x86_64', 'aarch64'],
     },
   },

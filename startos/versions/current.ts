5087903a6dbb9ee327d73d52e6009d242cdfde0a
lioranecho <support.satslist@gmail.com>
2026-09-24 15:06:10 -0400

    fix: point at working 0.1.15 image (data.json defaults + missing nostr-sdk dependency)

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '1.0.0:0',
+  version: '1.0.2:0',
   releaseNotes: {
     en_US: 'Initial release for StartOS',
     es_ES: 'Lanzamiento inicial para StartOS',
